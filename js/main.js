a23e61d21e9c0632c8c81d3828f77922dcc59ce8
codechappie <davidproyecto01@gmail.com>
2020-02-09 12:42:19 -0500

    avance

--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
-window.onload = function() {
-    // var cloneMenu = document.getElementById("menu").innerHTML;
-    // var menuContent = document.getElementById("mini__menu");
-    // var menuResponsive = document.getElementById("ident");
-    // var buttonMenu = document.getElementById("buttonmenu");
-    // var buttonClose = document.getElementById("buttonclose");
-  
-    // buttonMenu.addEventListener("click", function toogle() {
-    //   menuContent.innerHTML = cloneMenu;
-    //   menuResponsive.classList.toggle("show");
-    // });
-    // buttonClose.addEventListener("click", function closeMenu() {
-    //   menuResponsive.classList.remove("show");
-    // });
-  
-var myNav = document.getElementById('navbar');
+window.onload = function () {
+    const menuResponsive = document.getElementById("menu-responsive");
+    const buttonOpenMenu = document.getElementById("btn-open-menu");
+    const buttonCloseMenu = document.getElementById("btn-close-menu");
 
-  window.onscroll = function () { 
-      if (window.scrollY >= 110 ) {
-          myNav.classList.add("nav-colored");
-          myNav.classList.remove("nav-initial");
-      } 
-      else {
-          myNav.classList.add("nav-initial");
-          myNav.classList.remove("nav-colored");
-      }
-  };
+    buttonOpenMenu.addEventListener("click", function openMenu() {
+        menuResponsive.classList.add("slideInRight");
+        menuResponsive.classList.remove("slideOutRight");
+        menuResponsive.classList.remove("non-displayed");
+    });
+    menuResponsive.addEventListener('animationend', function() { 
+        console.log("animacion terminada");
+        menuResponsive.classList.remove("slideInRight");
+        buttonCloseMenu.addEventListener("click", function closeMenu() {        
+        menuResponsive.classList.add("slideOutRight");
+        })
+     });
+
+        buttonCloseMenu.addEventListener("hover", function closeMenu() {        
+            alert("hover");
+            
+            menuResponsive.classList.add("non-displayed");
+            menuResponsive.classList.remove("displayed");
+        });
     
-  
-  };
+
+    var myNav = document.getElementById('navbar');
+
+    window.onscroll = function () {
+        if (window.scrollY >= 110) {
+            myNav.classList.add("nav-colored");
+            myNav.classList.remove("nav-initial");
+        }
+        else {
+            myNav.classList.add("nav-initial");
+            myNav.classList.remove("nav-colored");
+        }
+    };
+
+
+};
